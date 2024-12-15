018cd60379beaf52365be4822ae8e1150c538afe
meustar <meustar829@gmail.com>
2024-12-16 05:04:33 +0900

    Project_movie wabapp backend start_initialization

---
+++ src/main/java/site/kgbmeu/kgb_movie/KgbMovieApplication.java
@@ -0,0 +1,13 @@
+package site.kgbmeu.kgb_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgbMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KgbMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/kgbmeu/kgb_movie/KgbMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package site.kgbmeu.kgb_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgbMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
