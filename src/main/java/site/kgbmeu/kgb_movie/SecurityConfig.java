75b9407b2e30728856ef37665653357fe55c4747
meustar <meustar829@gmail.com>
2024-12-23 22:53:07 +0900

    yml google 제외 주석처리, ãprovider 추가, ãSecurityConfig 수정, HomeController 추가

--- src/main/java/site/kgbmeu/kgb_movie/SecurityConfig.java
+++ src/main/java/site/kgbmeu/kgb_movie/SecurityConfig.java
@@ -1,18 +1,26 @@
 package site.kgbmeu.kgb_movie;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.oauth2Login()
-                .loginPage("/login")
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login") // 사용자 정의 로그인 페이지 (옵션)
+                        .defaultSuccessUrl("/") // 로그인 성공 후 리디렉션 경로
+                );
+        return http.build();
     }
 }
-
